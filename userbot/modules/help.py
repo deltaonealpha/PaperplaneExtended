8ad116670ed50b770eb6beb321f851e357abbbc7
Pranav Balaji <balaji.pranav@outlook.in>
2020-04-04 13:53:40 +0530

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -19,7 +19,7 @@ async def help(event):
         else:
             await event.edit("Please specify a valid module name.")
     else:
-        await event.edit("Please specify which module do you want help for !!\
+        await event.edit("Welcome to deta's ProBotOne Support page. Please specify which module do you want help for !!\
             \nUsage: .help <module name>")
         string = ""
         for i in CMD_HELP:
