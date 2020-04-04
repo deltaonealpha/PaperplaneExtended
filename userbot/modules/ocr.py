55bad7c7971559db90f3bd9c5e4ec2dd148d2138
Pranav Balaji <balaji.pranav@outlook.in>
2020-04-04 13:58:46 +0530

    Update ocr.py

--- userbot/modules/ocr.py
+++ userbot/modules/ocr.py
@@ -57,7 +57,7 @@ async def ocr(event):
     except BaseException:
         await event.edit("`Couldn't read it.`\n`I guess I need new glasses.`")
     else:
-        await event.edit(f"`Here's what I could read from it:`\n\n{ParsedText}"
+        await event.edit(f"`Sire, here's what I could read from it:`\n\n{ParsedText}"
                          )
     os.remove(downloaded_file_name)
 
